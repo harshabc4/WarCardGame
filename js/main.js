ba05047894ded6f686932ab9d282526d25e79372
Brian Harsha <harshabc4@gmail.com>
2022-04-23 07:18:57 -0400

    upload project

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//Example fetch using pokemonapi.co
+document.querySelector('button').addEventListener('click', getFetch)
+
+function getFetch(){
+  const choice = document.querySelector('input').value
+  const url = 'https://deckofcardsapi.com/api/deck/cs84z3ssk5dk/draw/?count=2'
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
+// cs84z3ssk5dk
